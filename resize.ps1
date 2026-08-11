9338fd7a9c3de52694e5d328ac47e6c23800f2ee
Mukid Hasan <seamrex@gmail.com>
2026-08-11 13:08:57 +0600

    clean

--- resize.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$img = [System.Drawing.Image]::FromFile("thumbnail.png")
-$newImg = New-Object System.Drawing.Bitmap(1200, 630)
-$g = [System.Drawing.Graphics]::FromImage($newImg)
-$g.DrawImage($img, 0, 0, 1200, 630)
-$newImg.Save("thumbnail.jpg", [System.Drawing.Imaging.ImageFormat]::Jpeg)
-$g.Dispose()
-$newImg.Dispose()
-$img.Dispose()
